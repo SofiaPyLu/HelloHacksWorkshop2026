28ba04b3e0be35647b325a868147408a47f1e5f0
SofiaPyLu <sofiapyloridis@gmail.com>
2026-09-24 18:52:07 -0700

    commit 1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Pokemon Battle Assistant</h1>
+    <p>What type of pokemon are you fighting?</p>
+    <button>Fire</button>
+<button>Water</button>
+<button>Grass</button>
+<button>Ground</button>
+    </>
+  )
+}
+
+export default App
